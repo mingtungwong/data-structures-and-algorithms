9b4fddc9dc2a96b0a4c416d94c421437f7c09b3f
Ming Tung Wong <mingtungwong@outlook.com>
2017-11-11 09:47:43 -0500

    Added documentation

--- ts/DynamicArray.ts
+++ ts/DynamicArray.ts
@@ -12,6 +12,13 @@ export class DynamicArray {
         this.currentIndex = 0;
     }
 
+    /**
+     * Adds value to the end of array. If index is provided, value is placed in
+     * that index. If index is before the last element then everything after
+     * the insertion is shifted down.
+     * @param value The value to insert into array
+     * @param index Optional, index location of array to insert value into
+     */
     add(value: any, index?: number): void {
         if(index !== undefined) {
             if(index < 0 || index >= this.capacity) throw new Error("Array Index Out of Bounds");
@@ -25,6 +32,11 @@ export class DynamicArray {
         if(this.currentIndex / this.capacity > this.threshold) this.doubleCapacity();
     }
 
+    /**
+     * Searches for the value in the array linearly and returns the index of
+     * the first occurence of value. Returns -1 if not found.
+     * @param value The value to search for
+     */
     indexOf(value: number): number {
         for(let i: number = 0; i < this.currentIndex; i++) {
             if(value === this.array[i]) return i;
@@ -32,6 +44,11 @@ export class DynamicArray {
         return -1;
     }
 
+    /**
+     * Searches for value linearly starting at the end of the array and returns
+     * the index of the first occurence of value. Returns -1 if not found.
+     * @param value The value to search for
+     */
     lastIndexOf(value: number): number {
         for(let i: number = this.currentIndex - 1; i >= 0; i--) {
             if(value === this.array[i]) return i;
@@ -39,16 +56,25 @@ export class DynamicArray {
         return -1;
     }
 
+    /**
+     * Returns the size/capacity of the array.
+     */
     size(): number {
         return this.capacity;
     }
 
+    /**
+     * Logs to console all the values in the array.
+     */
     print(): void {
         for(let value of this.array) {
             console.log(value);
         }
     }
 
+    /**
+     * Doubles the size/capacity of the array when the array surpasses the specified threshold.
+     */
     private doubleCapacity(): void {
         this.capacity *= 2;
         let newArray = new Array(this.capacity);
