8f9711d9491db00d49dde46bc76722e1cccc4faf
Ming Tung Wong <mingtungwong@outlook.com>
2017-11-14 18:49:19 -0500

    Begin implementation of queue

---
+++ ts/Queue.ts
@@ -0,0 +1,27 @@
+import { LinkedList } from './LinkedList';
+
+export class Queue {
+    private _queue: LinkedList;
+
+    constructor() {
+        this._queue = new LinkedList();
+    }
+
+    /**
+     * Adds the specified value to the end of the queue.
+     * @param value Value to add to the end of the queue.
+     */
+    enqueue(value: number) {
+        this._queue.addToEnd(value);
+    }
+
+    /**
+     * Returns the first element in the queue. Throws an
+     * error if the queue is empty.
+     */
+    dequeue(): number {
+        let result: number = this._queue.removeHead();
+        if(!result) throw new Error("The queue is empty!");
+        else return result;
+    }
+}
