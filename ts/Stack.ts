9e2e68bce1bdbe27c13449f43b47a9250bf7cc6b
Ming Tung Wong <mingtungwong@outlook.com>
2017-11-15 22:27:43 -0500

    Begin implementation of Stack

---
+++ ts/Stack.ts
@@ -0,0 +1,20 @@
+import { LinkedList } from './LinkedList';
+
+export class Stack {
+
+    private _stack: LinkedList;
+
+    constructor() {
+        this._stack = new LinkedList();
+    }
+
+    push(value: number) {
+        this._stack.addToEnd(value);
+    }
+
+    pop(): number {
+        let result: number = this._stack.removeTail();
+        if(result === null) throw new Error("The stack is empty!");
+        else return result;
+    }
+}
