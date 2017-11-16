9ef77e93d7e415ef6bbebdbbc4c20e37b907cc5e
Ming Tung Wong <mingtungwong@outlook.com>
2017-11-16 11:54:22 -0500

    Add peek method for Stack

--- ts/Stack.ts
+++ ts/Stack.ts
@@ -8,20 +8,40 @@ export class Stack {
         this._stack = new LinkedList();
     }
 
+    /**
+     * Pushes the value to the top of the stack.
+     * @param value Value to push onto Stack.
+     */
     push(value: number) {
         this._stack.addToEnd(value);
     }
 
+    /**
+     * Pops the value from the top of the stack and returns it.
+     */
     pop(): number {
         let result: number = this._stack.removeTail();
         if(result === null) throw new Error("The stack is empty!");
         else return result;
     }
 
+    /**
+     * Returns the value at the top of the stack without removing it.
+     */
+    peek(): number {
+        return this._stack.getTail();
+    }
+
+    /**
+     * Returns a string representation of the stack.
+     */
     toString(): string {
         return this._stack.toString().replace(' -> ', ', ');
     }
 
+    /**
+     * Outputs to console a string representation of the stack.
+     */
     print() {
         console.log(this.toString());
     }
